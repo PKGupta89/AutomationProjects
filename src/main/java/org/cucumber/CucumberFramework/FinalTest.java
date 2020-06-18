a12d3ca5c42f8df3a30c314cb00f487ac15faca9
P K Gupta <pkgupta2004@gmail.com>
2020-06-18 22:02:46 +0530

    Asian Development Tean Complted Projects

---
+++ src/main/java/org/cucumber/CucumberFramework/FinalTest.java
@@ -0,0 +1,13 @@
+package org.cucumber.CucumberFramework;
+
+public class FinalTest {
+
+	public void Completed() {
+		System.out.println("Project completed by Asian Development team");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Project completed by Asian Development team");
+	}
+
+}
